5a9c0083f358593b92fca0790d35a47663e310e8
rhonig22 <ryan.honig22@outlook.com>
2023-04-22 09:11:52 -0700

    AddStealth
    - add the enemy eye that sweeps its light
    - add raycasting logic for the eye to catch the player
    - add some new rooms
    - fix player animation bugs

--- GoldibloxInfinite/Assets/Scripts/LevelLoader.cs
+++ GoldibloxInfinite/Assets/Scripts/LevelLoader.cs
@@ -10,7 +10,7 @@ public class LevelLoader : MonoBehaviour
     private readonly int startLevel = 1;
     private readonly int startTimerLevel = 3;
     private readonly float waitTime = .25f;
-    private readonly int[] levelTypeList = new int[] { -1, -1, -1, 1, 2, 1, 3, 0, 1 };
+    private readonly int[] levelTypeList = new int[] { -1, -1, -1, 1, 2, 1, 3, 0, 1, 1, 2, 3 };
     private int[][] levelMap = new int[4][];
     private bool isLoading = false;
 
@@ -18,10 +18,10 @@ public class LevelLoader : MonoBehaviour
     void Start()
     {
         DataManager.currentLevel = SceneManager.GetActiveScene().buildIndex;
-        levelMap[0] = new int[] { 8 };
+        levelMap[0] = new int[] { 8, 11 };
         levelMap[1] = new int[] { 4, 6 };
-        levelMap[2] = new int[] { 5 };
-        levelMap[3] = new int[] { 7 };
+        levelMap[2] = new int[] { 5, 9 };
+        levelMap[3] = new int[] { 7, 10 };
         crossfadeAnimator = crossfade.GetComponent<Animator>();
         DataManager.gameOver.AddListener(GameOver);
     }
